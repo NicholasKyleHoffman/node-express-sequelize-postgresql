a5243eb61b7383adc69988819b1469551dadf48d
NicholasKyleHoffman <hoffman.nick89@gmail.com>
2020-05-03 14:14:41 -0700

    Added db.config.js file and config folder for postgresql & sequelize to work

---
+++ config/db.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  HOST : "localhost",
+  USER : "postgres",
+  PASSWORD : "123",
+  DB : "testdb",
+  dialect : "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+
+// Configure PostgreSQL database & Sequelize
+// First five parameters are for PostgreSQL connection.
+// pool is optional, it will be used for Sequelize connection pool configuration:
+//
+// max: maximum number of connection in pool
+// min: minimum number of connection in pool
+// idle: maximum time, in milliseconds, that a connection can be idle before being released
+// acquire: maximum time, in milliseconds, that pool will try to get connection before throwing error
+
+// API Ref for Sequelize Constructor:
+// https://sequelize.org/master/class/lib/sequelize.js~Sequelize.html#instance-constructor-constructor
