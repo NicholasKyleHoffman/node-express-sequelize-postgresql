66148d218441820493b017fb84ed30d5eb4ab685
NicholasKyleHoffman <hoffman.nick89@gmail.com>
2020-05-04 11:46:35 -0700

    added routes folder and routes.js file

---
+++ app/routes/tutorial.routes.js
@@ -0,0 +1,9 @@
+// Define HTTP Routes!!!
+//
+// When a client sends request for an endpoint using HTTP request (GET, POST, PUT, DELETE),
+// we need to determine how the server will reponse by setting up the routes.
+//
+// These are our routes:
+// /api/tutorials: GET, POST, DELETE
+// /api/tutorials/:id: GET, PUT, DELETE
+// /api/tutorials/published: GET
