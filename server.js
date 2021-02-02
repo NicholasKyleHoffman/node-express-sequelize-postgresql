29fffe3cd0aa16d067e31cecaea44ac24ed06da7
Nicholas Hoffman <NicholasKyleHoffman@users.noreply.github.com>
2021-02-01 18:18:25 -0800

    Fixed errors in server.js
    Now you can run "node server.js" from a cmd terminal. Output should be in parsed JSON format, including the text:
    " Message displaying from project's GET route! (See server.js for other route possibilites.)  "

--- server.js
+++ server.js
@@ -8,38 +8,18 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 
-// Problematic if not running on localhost (see readme or https://bezkoder.com/node-express-sequelize-postgresql/)
-// If Problematic => Comment below line
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/jspn
+// parse requests of content-type - application/json
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// FROM "INITIALIZE SEQUELIZE section https://bezkoder.com/node-express-sequelize-postgresql/"
-
-// Don’t forget to call sync() method in server.js:
-// In development, you may need to drop existing
-//  tables and re-sync database.
-// Just use force: true as following code:
-
-const db = require("./app/models");
-db.sequelize.sync();
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-
-// simple route (home or default)
+// simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to your FullStack App!." });
+  res.json({ message: "Message displaying from project's GET route! (See server.js for other route possibilites.)" });
 });
-
-// MUST include below before app.listen()
-require('./app/routes/turorial.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
